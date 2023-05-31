7dec8927cb2e79cf0c358d8e75ec2b04623b76fc
akberj <akbarj@gmail.com>
2023-05-31 09:02:13 +0500

    new start

---
+++ Bucky/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Bucky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    
+}
+
+
+// cool
